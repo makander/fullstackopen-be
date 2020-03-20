f4280cfc3d2adc8cfe99c913c6510b12a5997801
Nils Makander <makander@gmail.com>
2020-03-20 15:11:55 +0100

    uibuild

---
+++ build/precache-manifest.a46d94d1241f968af68bc57a231bf947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed365ac316cde74ab2bc2f860fa85c0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d7850428241aba4cd0",
+    "url": "/static/css/main.16221c59.chunk.css"
+  },
+  {
+    "revision": "481cea120914d9399566",
+    "url": "/static/js/2.d6178021.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d6178021.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71d7850428241aba4cd0",
+    "url": "/static/js/main.2060bfc1.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
